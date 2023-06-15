e3c5e3effe514299623235db7f1aa97ef3739370
yyyy2549 <yyyy2549@gmail.com>
2023-06-15 23:48:20 +0900

    장바구니 기능 구현 0.1

--- src/main/java/com/shop/controller/CartController.java
+++ src/main/java/com/shop/controller/CartController.java
@@ -1,13 +1,16 @@
 package com.shop.controller;
 
+import com.shop.dto.CartDetailDto;
 import com.shop.dto.CartItemDto;
 import com.shop.service.CartService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -22,6 +25,7 @@ public class CartController {
 
     private final CartService cartService;
 
+    //장바구니에에 담기
     @PostMapping("/cart")
     public  @ResponseBody ResponseEntity order(@RequestBody @Valid CartItemDto cartItemDto,
                                                BindingResult bindingResult, Principal principal){
@@ -54,4 +58,17 @@ public class CartController {
         return new ResponseEntity<Long>(cartItemId, HttpStatus.OK);
 
     }
+    
+    //장바구니 조회
+    @GetMapping(value = "/cart")
+    public String orderHist(Principal principal, Model model){
+
+        List<CartDetailDto> cartDetailDtoList = cartService.getCartList(principal.getName());
+
+        model.addAttribute("cartItems", cartDetailDtoList);
+
+        return "cart/cartList";
+    }
+
+
 }
